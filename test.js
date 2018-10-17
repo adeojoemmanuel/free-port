5b06a63c304cd1b1587fd0ca6ff07cb1f0475766
adeojoemmanuel <emmanuel.adeojo@yahoo.com>
2018-10-17 15:38:41 +0100

    updated test.js

--- test.js
+++ test.js
@@ -3,27 +3,8 @@
 
 const free = require('./')
 
-free(8090, () => {
-  expect(free).toBeDefined()
-})
+// free(8090, () => {
+//   expect(free).toBeDefined()
+// })
 
-
-// const args = require('get-them-args')(process.argv.slice(2))
-// const portres = args.port || args.unknown
-
-console.log(portres)
-
-// function checckobject(anyVar){
-// 	if(typeof anyVar == 'object' && anyVar instanceof Object && !(anyVar instanceof Array)){
-// 		return false
-// 	} else{
-// 		return true
-// 	}
-// }
-
-
-// if(checckobject(portres) == true){
-// 	console.log("true")
-// }else{
-// 	console.log("false")
-// }
+free('8090')
