b396eb202570add8d1a2b17d12f938b7af45e6ec
adeojoemmanuel <emmanuel.adeojo@yahoo.com>
2018-10-19 20:16:07 +0100

    updated function.js

--- function.js
+++ function.js
@@ -1,16 +1,6 @@
 #!/usr/bin/env node
 'use strict'
 
-// const shell = require('execa').shell
-
-// module.exports = function (port) {
-//   if (!Number.parseInt(port)) {
-//     return Promise.reject(new Error('Invalid argument for port'))
-//   }
-
-//   return shell(`lsof -i tcp:${port} | grep LISTEN | awk '{print $2}' | xargs kill -9`)
-// }
-
 const shell = require('execa').shell
 
 function freePort(port) {
